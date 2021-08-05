33b6ad153293f22343bfea2d026e42b0044c3941
Akash Kumar <akash.kumar@knoldus.com>
2021-08-05 11:56:12 +0530

    Update the person class

--- src/main/scala/com/knoldus/Person.scala
+++ src/main/scala/com/knoldus/Person.scala
@@ -1,6 +1,6 @@
 package com.knoldus
 
-
+//Class that compare the object on the basis of name and age
 class Person(val name : String, val age: Int) extends Ordered[Person] {
   override def compare(that: Person): Int = {
     if(this.name == that.name)
@@ -10,21 +10,25 @@ class Person(val name : String, val age: Int) extends Ordered[Person] {
   }
 }
 
+//Main obbject that perform the initialization and function call to class person
 object MainObject{
-  def main(args:Array[String]): Unit ={
-    val person1= new Person(name= "Test", age=24)
-    val person2= new Person(name= "Test", age=25)
-    val person3= new Person(name= "John1", age=24)
-    val person4= new Person(name= "John", age=25)
 
-    def compareObject(personOne: Person, personTwo: Person): Unit = {
-      if(personOne.compare(personTwo) > 0)
+  def main(args:Array[String]): Unit ={
+
+    val personOne= new Person(name= "John", age=29)
+    val personTwo= new Person(name= "John", age=30)
+    val personThree= new Person(name= "John1", age=29)
+    val personFour= new Person(name= "John", age=30)
+
+    def objectComparison(person1: Person, person2: Person): Unit = {
+      if(person1.compare(person2) > 0)
         println(false)
       else
         println(true)
     }
-    compareObject(person1,person2)
-    compareObject(person3,person4)
+    //function calling
+    objectComparison(personOne,personTwo)
+    objectComparison(personThree,personFour)
 
   }
 
